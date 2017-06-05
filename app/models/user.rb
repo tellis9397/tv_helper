a14cbf5fcc4a1c8fd8a1d334836fb322cbd11ba1
Thor <thornton.ellis@annkissam.com>
2017-06-05 10:58:42 -0400

    Fixtures stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
+
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
 	has_many :tv_shows, inverse_of: :user, dependent: :destroy
 
@@ -40,4 +43,17 @@ class User < ActiveRecord::Base
 	    SecureRandom.urlsafe_base64
 	  end
 	end
+
+	private
+
+		# Converts email to all lower-case.
+		def downcase_email
+		  self.email = email.downcase
+		end
+
+		# Creates and assigns the activation token and digest.
+		def create_activation_digest
+		  self.activation_token  = User.new_token
+		  self.activation_digest = User.digest(activation_token)
+		end
 end
